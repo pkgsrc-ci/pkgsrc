113be992b143e68f9cdd1ea3db4f63492398a0a6
Jonathan Perkin <jonathan@perkin.org.uk>
2024-07-18 10:32:22 +0100

    more debug

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -5,12 +5,15 @@ uname
 pwd
 ls
 env
+which gcc
+type gcc
+gcc -v
 cd ${GITHUB_WORKSPACE}/bootstrap
 ./bootstrap --prefix=${HOME}/pkg --unprivileged --workdir=${HOME}/wrkdir --binary-kit=/tmp/bootstrap.tar || true
 mkdir /tmp/foo
 echo hi >/tmp/foo/bar
-find /cygdrive/d/a/pkgsrc/pkgsrc/bootstrap/work
-cat /cygdrive/d/a/pkgsrc/pkgsrc/bootstrap/work/bmake/config.log
-ls /cygdrive/d/a/pkgsrc/pkgsrc/bootstrap/work/bmake/main.c
-head /cygdrive/d/a/pkgsrc/pkgsrc/bootstrap/work/bmake/main.c
+find $HOME/wrkdir/work
+cat $HOME/wrkdir/work/bmake/config.log
+ls $HOME/wrkdir/work/bmake/main.c
+head $HOME/wrkdir/work/bmake/main.c
 false
