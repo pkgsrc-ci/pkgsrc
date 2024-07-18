0d76d892a8a3a707f3b29e6adfb59603b72840e5
Jonathan Perkin <jonathan@perkin.org.uk>
2024-07-18 11:16:07 +0100

    consolidate

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
+#
+# Wrapper script helps to consolidate per-platform configuration.
+#
 
-set -x
-find /cygdrive/c/tools/cygwin
-PATH=/cygdrive/c/tools/cygwin/usr/bin:/cygdrive/c/tools/cygwin/bin
-uname
-pwd
-ls
-env
-which gcc
-type gcc
-gcc -v
+case "$(uname)" in
+CYGWIN*)
+	PATH=/cygdrive/c/tools/cygwin/bin
+	;;
+esac
+
 cd ${GITHUB_WORKSPACE}/bootstrap
-./bootstrap --prefix=${HOME}/pkg --unprivileged --workdir=${HOME}/wrkdir --binary-kit=/tmp/bootstrap.tar || true
-mkdir /tmp/foo
-echo hi >/tmp/foo/bar
-find $HOME/wrkdir/work
-cat $HOME/wrkdir/work/bmake/config.log
-ls $HOME/wrkdir/work/bmake/main.c
-head $HOME/wrkdir/work/bmake/main.c
-false
+./bootstrap \
+	--binary-kit=${HOME}/bootstrap.tar \
+	--make-jobs=4 \
+	--prefix=${HOME}/pkg \
+	--unprivileged \
+	--workdir=${HOME}/wrkdir
