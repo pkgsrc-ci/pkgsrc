912095f46d568525a9dc67136c82fb394350bb50
Jonathan Perkin <jonathan@perkin.org.uk>
2024-07-18 13:12:59 +0100

    support bsd?

--- .github/scripts/bootstrap.sh
+++ .github/scripts/bootstrap.sh
@@ -3,9 +3,16 @@
 # Wrapper script helps to consolidate per-platform configuration.
 #
 
+BS_PREFIX="${HOME}/pkg"
+BS_UNPRIVILEGED="--unprivileged"
+
 case "$(uname)" in
 CYGWIN*)
 	PATH=/cygdrive/c/tools/cygwin/bin
+	;;
+*BSD)
+	BS_PREFIX=/usr/pkg
+	BS_UNPRIVILEGED=
 	;;
 esac
 
@@ -13,6 +20,6 @@ cd ${GITHUB_WORKSPACE}/bootstrap
 ./bootstrap \
 	--binary-kit=${HOME}/bootstrap.tar \
 	--make-jobs=4 \
-	--prefix=${HOME}/pkg \
-	--unprivileged \
+	--prefix=${BS_PREFIX} \
+	${BS_UNPRIVILEGED} \
 	--workdir=${HOME}/wrkdir
