b8f1fd05a1905dbc71983426d6c99e3c94ec9f69
Jonathan Perkin <jonathan@perkin.org.uk>
2024-07-18 13:17:13 +0100

    revert

--- .github/scripts/bootstrap.sh
+++ .github/scripts/bootstrap.sh
@@ -3,16 +3,9 @@
 # Wrapper script helps to consolidate per-platform configuration.
 #
 
-BS_PREFIX="${HOME}/pkg"
-BS_UNPRIVILEGED="--unprivileged"
-
 case "$(uname)" in
 CYGWIN*)
 	PATH=/cygdrive/c/tools/cygwin/bin
-	;;
-*BSD)
-	BS_PREFIX=/usr/pkg
-	BS_UNPRIVILEGED=
 	;;
 esac
 
@@ -20,6 +13,6 @@ cd ${GITHUB_WORKSPACE}/bootstrap
 ./bootstrap \
 	--binary-kit=${HOME}/bootstrap.tar \
 	--make-jobs=4 \
-	--prefix=${BS_PREFIX} \
-	${BS_UNPRIVILEGED} \
+	--prefix=${HOME}/pkg \
+	--unprivileged \
 	--workdir=${HOME}/wrkdir
