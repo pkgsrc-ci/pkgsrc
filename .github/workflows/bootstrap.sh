8352867193b1affa70c2e6c7b7e6bdfcf545e5ca
Jonathan Perkin <jonathan@perkin.org.uk>
2024-07-18 10:26:47 +0100

    more

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -6,7 +6,9 @@ pwd
 ls
 env
 cd ${GITHUB_WORKSPACE}/bootstrap
-./bootstrap --prefix=${HOME}/pkg --unprivileged --workdir=${TMP}/bootstrap --binary-kit=/tmp/bootstrap.tar || true
+./bootstrap --prefix=${HOME}/pkg --unprivileged --workdir=${HOME}/wrkdir --binary-kit=/tmp/bootstrap.tar || true
+mkdir /tmp/foo
+echo hi >/tmp/foo/bar
 find /cygdrive/d/a/pkgsrc/pkgsrc/bootstrap/work
 cat /cygdrive/d/a/pkgsrc/pkgsrc/bootstrap/work/bmake/config.log
 ls /cygdrive/d/a/pkgsrc/pkgsrc/bootstrap/work/bmake/main.c
